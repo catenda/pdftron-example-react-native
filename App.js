850a25a3a4ee123c2a2c66abba588c355b0e610c
Mark Holland <mark.holland@catenda.no>
2021-06-30 14:48:19 +0200

    Tablet annotation toolbar

--- App.js
+++ App.js
@@ -5,8 +5,7 @@ import {Config, DocumentView, RNPdftron} from 'react-native-pdftron';
 const App = () => {
   RNPdftron.initialize('KEY_GOES_HERE');
 
-  const path =
-    'https://pdftron.s3.amazonaws.com/custom/websitefiles/android/blog/signing/Screenshot_1622044031.png';
+  const path = 'https://pdftron.s3.amazonaws.com/downloads/pdfref.pdf';
 
   const toolbar = {
     [Config.CustomToolbarKey.Id]: 'toolbar',
@@ -27,6 +26,7 @@ const App = () => {
         Config.AnnotationMenu.delete,
         Config.AnnotationMenu.editText,
       ]}
+      disabledElements={[Config.Buttons.editMenuButton]}
       followSystemDarkMode={false}
       bottomToolbarEnabled={false}
       document={path}
