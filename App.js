bbc6dbce38475127377dd487ee5b9da397ece1ff
Mark Holland <mark.holland@catenda.no>
2022-06-13 12:44:39 +0200

    Open file from local path

--- App.js
+++ App.js
@@ -5,6 +5,7 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {RNPdftron} from 'react-native-pdftron';
 
 import DocumentScreen from './App/Screens/DocumentScreen.js';
+import DocumentFromLocalFileScreen from './App/Screens/DocumentFromLocalFileScreen.js';
 import DocumentListScreen from './App/Screens/DocumentListScreen.js';
 import HomeScreen from './App/Screens/HomeScreen.js';
 
@@ -14,6 +15,10 @@ const Tab = createBottomTabNavigator();
 const DocumentStack = () => (
   <Stack.Navigator initialRouteName="DocumentList">
     <Stack.Screen name="Document" component={DocumentScreen} />
+    <Stack.Screen
+      name="DocumentFromLocalFileScreen"
+      component={DocumentFromLocalFileScreen}
+    />
     <Stack.Screen name="DocumentList" component={DocumentListScreen} />
   </Stack.Navigator>
 );
