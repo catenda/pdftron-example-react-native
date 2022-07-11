be4fae80aec52ec9860449e205c497ed0428d5e1
Mark Holland <mark.holland@catenda.no>
2022-07-11 13:49:35 +0200

    Update android script

--- scripts/android.sh
+++ scripts/android.sh
@@ -7,20 +7,17 @@ trap "exit" INT
 if command -v adb &> /dev/null; then
   echo "adb is available"
   numberOfConnectedDevices=$(adb devices | grep -o -w 'device' | wc -l)
-  if [ $numberOfConnectedDevices -eq 0 ]; then
-    echo "No connected devices, skipping uninstall"
-  elif [ "$numberOfConnectedDevices" -eq 1 ]; then
-    echo "Device is connected"
-    isAppInstalled=$(adb shell pm list packages 2>/dev/null | grep com.pdftronexample)
+  devices=$(adb devices | grep -v devices | grep device | cut -f 1)
+  for device in $devices
+  do
+    isAppInstalled=$(adb -s $device shell pm list packages 2>/dev/null | grep com.pdftronexample)
     if [ -n "$isAppInstalled" ]; then
-      echo "Removing existing build from device"
-      adb uninstall com.pdftronexample
+      echo "Removing existing build from $device"
+      adb -s $device uninstall com.pdftronexample
     else
-      echo "App is not installed, skipping uninstall"
+      echo "App is not installed on $device, skipping uninstall"
     fi
-  elif [ "$numberOfConnectedDevices" -gt 1 ]; then
-    echo "More than one device is connected, skipping uninstall"
-  fi
+  done
 fi
 
-yarn react-native run-android "$@"
+yarn react-native run-android --active-arch-only "$@"
