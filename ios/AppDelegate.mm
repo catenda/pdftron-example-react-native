c74322972723ba1b2cb2a0ca7e9ab8503aa814ac
Mark Holland <mark.holland@catenda.no>
2023-07-12 11:20:01 +0200

    Set font selection header appearance

--- ios/AppDelegate.mm
+++ ios/AppDelegate.mm
@@ -1,6 +1,7 @@
 #import "AppDelegate.h"
 
 #import <React/RCTBundleURLProvider.h>
+#import <Tools/Tools.h> // /!\ react-native-pdftron
 
 @implementation AppDelegate
 
@@ -10,6 +11,16 @@
   // You can add your custom initial props in the dictionary below.
   // They will be passed down to the ViewController used by React Native.
   self.initialProps = @{};
+  
+  /**
+   /!\ react-native-pdftron
+   Font selection header
+   */
+  UINavigationBarAppearance *appearance = [[UINavigationBarAppearance alloc] init];
+  [appearance configureWithOpaqueBackground];
+  [UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[PTPopoverNavigationController class]]].standardAppearance = appearance;
+  [UINavigationBar appearanceWhenContainedInInstancesOfClasses:@[[PTPopoverNavigationController class]]].scrollEdgeAppearance = appearance;
+
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
 }
 
